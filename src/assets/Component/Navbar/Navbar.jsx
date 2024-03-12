9dc57f813753106ad00be8fddd0b8e6e8efc62af
hoangquocvietuet@gmail.com <hoangquocvietuet@gmail.com>
2024-03-12 14:42:22 +0700

    fixed

--- src/assets/Component/Navbar/Navbar.jsx
+++ src/assets/Component/Navbar/Navbar.jsx
@@ -6,17 +6,8 @@ const Navbar = () => {
   const navigate = useNavigate();
   return (
     <div className="Navbar">
-      <div className="NavLeft">
-        <img src="/Amoeba.jfif" alt="Logo" className="logo" />
-        <h1 className="Name">Amoeba</h1>
-        <div className="Pages">
-          <span onClick={navigate("/create")}>Create</span>
-          <span onClick={navigate("/collections")}>Collections</span>
-        </div>
-      </div>
-      {/* <div className="NavRight">
-        <div className="ConnectWalletBtn">Connect Wallet</div>
-      </div> */}
+      <img src="/Amoeba.jfif" className="logo" />
+      
     </div>
   );
 };
